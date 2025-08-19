749a77a5a7e614bd9c3b78aff9e505e45b265b7f
shaheer hussain <shaheer@aviatrix.com>
2025-08-19 09:43:13 -0600

    add controller nsg rule

--- avx_azure_controller8_copilot/main.tf
+++ avx_azure_controller8_copilot/main.tf
@@ -191,3 +191,28 @@ resource "azurerm_network_interface_security_group_association" "cp_nic_nsg" {
   network_security_group_id = azurerm_network_security_group.cp_nsg.id
 }
 
+resource "azurerm_network_security_group" "ct_nsg" {
+  name                = "${var.name_prefix}-ct-nsg"
+  location            = var.location
+  resource_group_name = azurerm_resource_group.rg.name
+}
+
+resource "azurerm_network_security_rule" "https_rule" {
+  name                        = "Allow-HTTPS"
+  priority                    = 100
+  direction                   = "Inbound"
+  access                      = "Allow"
+  protocol                    = "Tcp"
+  source_port_range           = "*"
+  destination_port_range      = "443"
+  source_address_prefix       = "*"
+  destination_address_prefix  = "*"
+  resource_group_name         = azurerm_resource_group.rg.name
+  network_security_group_name = azurerm_network_security_group.ct_nsg.name
+}
+
+resource "azurerm_network_interface_security_group_association" "ct_nic_nsg" {
+  network_interface_id      = azurerm_network_interface.ct_nic.id
+  network_security_group_id = azurerm_network_security_group.ct_nsg.id
+}
+
