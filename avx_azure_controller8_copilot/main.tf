459f53b13f1d7029004b36f6d3e10f27a1dfbaf0
shaheer hussain <shaheer@aviatrix.com>
2025-08-19 10:05:14 -0600

    fix typo

--- avx_azure_controller8_copilot/main.tf
+++ avx_azure_controller8_copilot/main.tf
@@ -149,7 +149,7 @@ resource "azurerm_managed_disk" "data_disk" {
 
 resource "azurerm_virtual_machine_data_disk_attachment" "vm_disk_attach" {
   managed_disk_id    = azurerm_managed_disk.data_disk.id
-  virtual_machine_id = var.azurerm_linux_virtual_machine.cp.id
+  virtual_machine_id = azurerm_linux_virtual_machine.cp.id
   lun                = 0
   caching            = "ReadWrite"
 }
